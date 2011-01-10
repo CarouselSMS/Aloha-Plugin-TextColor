8648cc43ed2ae5b56943067bce01e747581d23ac
Edward Tsech <edtsech@gmail.com>
2011-01-10 16:59:37 +0600

    Added css file

--- plugin.js
+++ plugin.js
@@ -6,31 +6,68 @@ GENTICS.Aloha.TextColor = new GENTICS.Aloha.Plugin('com.gentics.aloha.plugins.Te
 /**
  * Configure the available languages
  */
-GENTICS.Aloha.TextColor.languages = ['en'];
+GENTICS.Aloha.TextColor.languages = ['en', 'ru'];
 
 /**
  * Initialize the plugin and set initialize flag on true
  */
-GENTICS.Aloha.TextColor.init = function () {
-  // create a new button
-  var that = this;
+GENTICS.Aloha.TextColor.init = function () {	
+	
+  var that = this,
+      buttons = {},
+      colors = ['black', 'red', 'green', 'blue'],
+      stylePath = GENTICS_Aloha_base + '/plugins/com.gentics.aloha.plugins.TextColor/css/TextColor.css';
+      
+  jQuery('<link rel="stylesheet" />').attr('href', stylePath).appendTo('head');
+  
+  jQuery.each(colors, function(index, value){
+    buttons[value] = new GENTICS.Aloha.ui.Button({
+      "iconClass" : "GENTICS_button_" + value,
+      "size" : "small",
+      "onclick": function () {
+        if (GENTICS.Aloha.activeEditable) {
+  				GENTICS.Aloha.activeEditable.obj[0].focus()
+  			}
+  			var markup = jQuery('<span style="color:' + value + '"></span>');
+  			var rangeObject = GENTICS.Aloha.Selection.rangeObject;
+  			var foundMarkup = rangeObject.findMarkup(function() {
+  				return this.nodeName.toLowerCase() == markup.get(0).nodeName.toLowerCase()
+  			},
+  			GENTICS.Aloha.activeEditable.obj);
 
-  var button = new GENTICS.Aloha.ui.Button({
-    'iconClass' : 'GENTICS_button_c',
-    'size' : 'small',
-    'onclick': function () {
-      var rangeObject = GENTICS.Aloha.Selection.rangeObject;
-      var markup = jQuery('<span style="color: red;"></span>');
-      GENTICS.Utils.Dom.addMarkup(rangeObject, markup);
-    },
-    'tooltip' : that.i18n('button.textcolor.tooltip')
+  			if (foundMarkup) {
+  				if (rangeObject.isCollapsed()) {
+  					GENTICS.Utils.Dom.removeFromDOM(foundMarkup, rangeObject, true)
+  				} else {
+  					GENTICS.Utils.Dom.removeMarkup(rangeObject, markup, GENTICS.Aloha.activeEditable.obj)
+  				}
+  			} else {
+  				if (rangeObject.isCollapsed()) {
+  					GENTICS.Utils.Dom.extendToWord(rangeObject)
+  				}
+  				GENTICS.Utils.Dom.addMarkup(rangeObject, markup)
+  			}
+  			rangeObject.select();
+  			return false
+      },
+      "tooltip" : that.i18n("button.textcolor." + value + ".tooltip")
+    });
   });
 
-  // add it to the floating menu
-  GENTICS.Aloha.FloatingMenu.addButton(
-    'GENTICS.Aloha.continuoustext',
-    button,
-    GENTICS.Aloha.i18n(GENTICS.Aloha, 'floatingmenu.tab.format'),
-    1
-  );
+  // // add it to the floating menu
+  // GENTICS.Aloha.FloatingMenu.addButton(
+  //   'GENTICS.Aloha.continuoustext',
+  //   button,
+  //   GENTICS.Aloha.i18n(GENTICS.Aloha, 'floatingmenu.tab.format'),
+  //   1
+  // );
+  
+  jQuery.each(colors, function(index, value){
+    GENTICS.Aloha.FloatingMenu.addButton(
+      "GENTICS.Aloha.continuoustext", 
+      buttons[value], 
+      that.i18n("floatingmenu.tab.color"), 
+      1
+    );    
+  });
 };
